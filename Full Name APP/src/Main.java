f9454a017e9300dbe147000abf1912403bbd1bf0
jsonnek <jtsonnek@students.unwsp.edu>
2026-02-27 19:32:03 -0600

    Fixed to Full name app to be more efficent

--- Full Name APP/src/Main.java
+++ Full Name APP/src/Main.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 public class Main {
     static Scanner userInput = new Scanner(System.in);
     public static void main(String[] args) {
-        String firstName, middleName, lastName, fullName;
+        StringBuilder name = new StringBuilder();
         System.out.println("What is your first name?");
-        firstName = userInput.nextLine();
+        name.append(userInput.nextLine());
         System.out.println("What is your middle name?");
-        middleName = userInput.nextLine();
+        name.append(userInput.nextLine());
         System.out.println("What is your last name?");
-        lastName = userInput.nextLine();
-        fullName = firstName + " " + middleName + " " + lastName;
-        System.out.println("Your full name is: " + fullName);
+        name.append(userInput.nextLine());
+        System.out.println("Your full name is: " + name.toString());
     }
 }
